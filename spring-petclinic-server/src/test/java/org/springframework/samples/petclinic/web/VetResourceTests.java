5b2704cd036457d806868d62a3d64056e85671d1
Jorge Balderas <jorgerbf@gmail.com>
2025-09-29 15:37:31 -0400

    Upgrade project to Spring Boot 3.3.x, Java 21 using openrewrite.

--- spring-petclinic-server/src/test/java/org/springframework/samples/petclinic/web/VetResourceTests.java
+++ spring-petclinic-server/src/test/java/org/springframework/samples/petclinic/web/VetResourceTests.java
@@ -1,14 +1,12 @@
 package org.springframework.samples.petclinic.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.service.ClinicService;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -18,7 +16,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(VetResource.class)
 public class VetResourceTests {
 
