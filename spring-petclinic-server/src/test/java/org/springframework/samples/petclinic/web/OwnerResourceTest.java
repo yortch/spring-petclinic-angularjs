3aacb7f94724e91c4305144282c695449d32c4b7
Jorge Balderas <jorgerbf@gmail.com>
2025-10-01 10:52:09 -0400

    Upgrade Spring Boot to 3.4.1 and replace deprecated @MockBean with @MockitoBean

--- spring-petclinic-server/src/test/java/org/springframework/samples/petclinic/web/OwnerResourceTest.java
+++ spring-petclinic-server/src/test/java/org/springframework/samples/petclinic/web/OwnerResourceTest.java
@@ -44,7 +44,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
@@ -72,7 +72,7 @@ public class OwnerResourceTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ClinicService clinicService;
 
     private Owner validOwner;
