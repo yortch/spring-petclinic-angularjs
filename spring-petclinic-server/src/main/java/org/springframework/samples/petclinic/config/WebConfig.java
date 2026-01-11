49d3cea51b730f34e023820083f780c7aee30201
Jorge Balderas <jorgerbf@gmail.com>
2026-01-11 13:06:59 -0500

    Add AngularJS client dependency and update datasource initialization properties

--- spring-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/WebConfig.java
+++ spring-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/");
     }
 }
